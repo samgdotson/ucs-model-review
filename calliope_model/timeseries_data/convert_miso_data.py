a91723fd92d17e5dd72077ac9437e2210cf83511
samgdotson <sdotson@ucsusa.org>
2024-03-18 14:03:23 -0500

    adds miso data converter

---
+++ calliope_model/timeseries_data/convert_miso_data.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    import pandas as pd
+    from pathlib import Path
+    import os
+    cwd = Path(os.path.dirname(__file__))
+    energy_data_path = Path(list(cwd.parents)[2])/'energy_data'
+    read_path = energy_data_path/"2023-miso-demand.csv"
+    print(f"Reading from {read_path}")
+    miso_data = pd.read_csv(read_path, 
+                            index_col='Interval End', 
+                            parse_dates=True)
+    miso_data['Load'] = miso_data['Load']*-1
+    save_path = cwd/"miso_demand_calliope.csv"
+    print(f"Saving to {save_path}")
+    miso_data.to_csv(save_path)
